f6091c98f34cb63299cd39c6f01d604f71785e2e
Vniteen <niteen@vibeosys.com>
2016-01-01 12:28:38 +0530

    UserId is added in google analytics plugin

--- wp-content/plugins/vibeosys-google-analytics.php
+++ wp-content/plugins/vibeosys-google-analytics.php
@@ -9,6 +9,25 @@ Version: 1.0
 
 function vibeosys_google_analytics() { ?>
 <script>
+    function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+    function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)===' ') c = c.substring(1);
+        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+    }
+    return "";
+    }
+    var TempId = getCookie("Vb_Id");
+    
+    if(!TempId){
     var random = function(){
         return Math.floor(Math.random() * 3);
     };
@@ -19,14 +38,17 @@ function vibeosys_google_analytics() { ?>
      var randomUserId = function(){
          return prefix[random()] + '-' + middle[random()] + '-' + suffix[random()];
      };
-     
+     var TempId = randomUserId();
+     setCookie("Vb_Id", TempId);
+    }
+   
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-71829158-1', 'auto');
-  var randomId = randomUserId();
+  var randomId = TempId;
   ga('set', 'userId', randomId);
   ga('require', 'displayfeatures');
   ga('send', 'pageview');
