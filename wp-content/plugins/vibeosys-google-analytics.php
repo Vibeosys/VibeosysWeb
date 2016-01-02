6a70a37ee180679505ab9f6ae3823e5937b18383
anandvb <anand@vibeosys.com>
2016-01-02 18:05:39 +0530

    Added inspectlet videos
    Added user behaviour testing

--- wp-content/plugins/vibeosys-google-analytics.php
+++ wp-content/plugins/vibeosys-google-analytics.php
@@ -55,6 +55,16 @@ function vibeosys_google_analytics() { ?>
 
 </script>
 
+<!-- Begin Inspectlet Embed Code -->
+<script type="text/javascript" id="inspectletjs">
+window.__insp = window.__insp || [];
+__insp.push(['wid', 316412001]);
+(function() {
+function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+})();
+</script>
+<!-- End Inspectlet Embed Code -->
 
 <?php }
 add_action( 'wp_head', 'vibeosys_google_analytics', 10 );
