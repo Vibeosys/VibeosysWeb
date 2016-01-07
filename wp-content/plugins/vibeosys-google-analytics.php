475146e3277484189dc1958c8a72d62bc95ec3fe
Vniteen <niteen@vibeosys.com>
2016-01-07 13:47:37 +0530

    google analytic plugin modify with try-catch block
    guid generator is used to generate UserId

--- wp-content/plugins/vibeosys-google-analytics.php
+++ wp-content/plugins/vibeosys-google-analytics.php
@@ -9,39 +9,49 @@ Version: 1.0
 
 function vibeosys_google_analytics() { ?>
 <script>
-    function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+   function setCookie(cname, cvalue) {
+        try{
+            document.cookie = cname + "=" + cvalue;
+        }catch(ex){
+            alert('error while set cookie');
+        }
     }
     function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)===' ') c = c.substring(1);
-        if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
-    }
+       try{
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)===' ') c = c.substring(1);
+            if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+        }
+      }catch(ex){
+            alert('Error occurd while reading cookie');
+   		}
     return "";
     }
     var TempId = getCookie("Vb_Id");
-    
-    if(!TempId){
-    var random = function(){
-        return Math.floor(Math.random() * 3);
-    };
-     var prefix = ['abc','def','ghi'];
-     var middle = ['123','456','768'];
-     var suffix = ['rst','uvw','xyz'];
-     
-     var randomUserId = function(){
-         return prefix[random()] + '-' + middle[random()] + '-' + suffix[random()];
-     };
-     var TempId = randomUserId();
-     setCookie("Vb_Id", TempId);
+    function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                        .toString(16)
+                        .substring(1);
     }
-   
+    if(!TempId){
+       
+        var randomUserId = function(){
+           try{
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                    s4() + '-' + s4() + s4() + s4();
+            }catch(ex){
+                alert('Error while generating Guid');
+            }
+        };
+        
+        var TempId = randomUserId();
+        
+        setCookie("Vb_Id", TempId);
+    }
+  
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -55,16 +65,15 @@ function vibeosys_google_analytics() { ?>
 
 </script>
 
-<!-- Begin Inspectlet Embed Code -->
 <script type="text/javascript" id="inspectletjs">
 window.__insp = window.__insp || [];
 __insp.push(['wid', 316412001]);
 (function() {
-function ldinsp(){if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
-setTimeout(ldinsp, 500); document.readyState != "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
+function ldinsp(){if(typeof window.__inspld !== "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' === document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+setTimeout(ldinsp, 500); document.readyState !== "complete" ? (window.attachEvent ? window.attachEvent('onload', ldinsp) : window.addEventListener('load', ldinsp, false)) : ldinsp();
 })();
 </script>
-<!-- End Inspectlet Embed Code -->
+
 
 <?php }
 add_action( 'wp_head', 'vibeosys_google_analytics', 10 );
